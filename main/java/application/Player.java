41b14efa9ba8628a7de2862e703e9ddc9d4c48de
maxi2011816 <38374634+maxi2011816@users.noreply.github.com>
2020-03-24 02:21:23 +0100

    Update Player.java
    1

--- main/java/application/Player.java
+++ main/java/application/Player.java
@@ -2,37 +2,68 @@ package application;
 
 import org.telegram.abilitybots.api.sender.SilentSender;
 
-import java.util.LinkedList;
+import java.util.*;
 
 public class Player {
+    private final String name;
+    private final long ID;
+    
+    private Role role;
+    private List<Card> cards;
+    private boolean hasKey;
+    
     private static LinkedList<Player> players;
     private Game currentGame;
-    private String name;
-    private long ID;
     public SilentSender silent;
 
 
-    Player(long ID, Game game){
+    public Player(long ID, Game game, List<Card> cards, boolean hasKey){
         if (players==null){
             players=new LinkedList<>();
         }
         players.add(this);
         this.currentGame = game;
         this.ID=ID;
+        this.cards = new ArrayList<>();
+        this.hasKey = false;
+    }
+    
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void setHasKey(boolean hasKey) {
+        this.hasKey = hasKey;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public void setSilent(SilentSender silent){
         this.silent = silent;
     }
 
-    public Game getGame(){
-        return currentGame;
-    }
-
     public void setName(String name){
         this.name=name;
         currentGame.silent.send("Dein Name wurde festgelegt, " + name, getID());
         currentGame.addPlayer(this);
+    } 
+    
+    public List<Card> getCards() {
+        return cards;
+    }
+    
+    public Role getRole() {
+        return role;
+    }
+    
+    public boolean isHasKey() {
+        return hasKey;
+    }
+    
+    public Game getGame(){
+        return currentGame;
     }
 
     public String getName(){
